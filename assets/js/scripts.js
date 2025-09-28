1ff6e75d51b6f9e022dbfb30eda8ffa4575f61e4
TimOsahenru <timosahenru@gmail.com>
2025-09-28 18:26:21 +0000

    dj section 95% ready

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
-document.querySelector(".nav-toggler").addEventListener("click", function() {
-    document.querySelector("nav").classList.toggle("active");
-    document.body.classList.toggle("nav-active");
-    body
-});
+
+       // Mobile navigation toggle
+        document.querySelector('.nav-toggler').addEventListener('click', function() {
+            document.querySelector('nav').classList.toggle('active');
+        });
